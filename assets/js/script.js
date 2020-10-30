3f4fd36cb1f87e628165013ca618df2f7b5704c4
Paulina Murzanska <p.murzanska@gmail.com>
2020-10-30 09:06:06 +0100

    media query and JS init

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// console.log("czy działa JS?");
+
+// alert("kominikat")
+
+
+let navigation = document.getElementById("navigation");
+console.dir(navigation);
+
+window.addEventListener("scroll", function () {
+    let scrollYposition = window.pageYOffset
+    // console.log(scrollYposition);
+
+    if (scrollYposition >= 100) {
+        navigation.classList.add("small");
+    } else {
+        navigation.classList.remove("small");
+
+    }
+
+})
+
+
+let hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+
+    hamburger.classList.toggle("active");
+
+    navigation.classList.toggle("menu-open");
+
+
+
+})
